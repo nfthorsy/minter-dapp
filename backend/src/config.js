37b0c7bcf7d5d75ae3d15078d5574e81c8aae606
nfthorsy <faycal.benhaij@live.fr>
2022-07-17 18:14:26 +0200

    rinkeby teste

--- backend/src/config.js
+++ backend/src/config.js
@@ -43,7 +43,7 @@ const format = {
 };
 
 const extraMetadata = {
-  external_url: "https://codecats.xyz", // Replace with your website or remove this line if you do not have one.
+  external_url: "www.boldhorsy.xyz", // Replace with your website or remove this line if you do not have one.
 };
 
 // NFTPort Info
